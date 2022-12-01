7fa02c84d20390b2099b855c5262572e04938c64
Louis Vannoorenberghe <louis.vannoo@gmail.com>
2022-12-01 21:19:12 +0100

    add color selector

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const CONTAINER_DIM = 900;
 
 const containerDiv = document.querySelector("div.container");
 const inputResolution = document.querySelector("input#res");
-const resetButton = document.querySelector("button");
+const resetButton = document.querySelector("button#reset");
+const colorSelector = document.querySelector("input#colorSelector");
 
 let mouseDown = false;
 
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
-containerDiv.addEventListener("mouseover", changeColor)
-containerDiv.addEventListener("mouseover", changeColor)
-resetButton.addEventListener("click", resetGrid)
+containerDiv.addEventListener("mouseover", changeColor);
+containerDiv.addEventListener("mouseover", changeColor);
+resetButton.addEventListener("click", resetGrid);
 
 function genGrid(resolution) {
     for (let i =0; i<resolution*resolution; i++) {
@@ -27,7 +28,7 @@ function cleanGrid(){
 
 function changeColor(e){
     if (e.type === 'mouseover' && mouseDown)
-        {e.target.classList.add('blue')}
+        {e.target.style.backgroundColor = colorSelector.value}
 }
 
 function resetGrid(){
