6022ae284662e0ccfe5f0a68ec0e125abcd0664e
Louis Vannoorenberghe <louis.vannoo@gmail.com>
2022-12-01 19:48:25 +0100

    add click + mouseover to change pixel, cleaned code

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let squareDiv;
+const CONTAINER_DIM = 900;
+
 const containerDiv = document.querySelector("div.container");
-const containerDimension = 900;
-const rangeRes = document.querySelector("input#res");
-let res = 50;
-let squareNumber = res*res;
-let allSquares;
+const inputResolution = document.querySelector("input#res");
+const resetButton = document.querySelector("button");
 
+let mouseDown = false;
 
-function genGrid() {
-    for (let i =0; i<squareNumber; i++) {
-    squareDiv = document.createElement('div');
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+containerDiv.addEventListener("mouseover", changeColor)
+containerDiv.addEventListener("mouseover", changeColor)
+resetButton.addEventListener("click", resetGrid)
+
+function genGrid(resolution) {
+    for (let i =0; i<resolution*resolution; i++) {
+    const squareDiv = document.createElement('div');
     squareDiv.classList.add('square'); 
-    squareDiv.style.cssText = `height: ${containerDimension/res}px; width: ${containerDimension/res}px`;
+    squareDiv.style.cssText = `height: ${CONTAINER_DIM/resolution}px; width: ${CONTAINER_DIM/resolution}px`;
     containerDiv.appendChild(squareDiv);
 }
 }
 
-genGrid();
+function cleanGrid(){
+    containerDiv.innerHTML = '';
+}
 
-containerDiv.style.cssText = `height: ${containerDimension}px; width: ${containerDimension}px`;
+function changeColor(e){
+    if (e.type === 'mouseover' && mouseDown)
+        {e.target.classList.add('blue')}
+}
 
-containerDiv.addEventListener("mouseover", function( event ) {
-    event.target.classList.add('blue')})
+function resetGrid(){
+    cleanGrid();
+    genGrid(inputResolution.value);
+}
 
-const resetButton = document.querySelector("button")
-
-resetButton.addEventListener("click", () => {
-    res = rangeRes.value;
-    console.log(res)
-    squareNumber = res*res;
-    allSquares = document.querySelectorAll("div.square");
-    allSquares.forEach(square => {
-        containerDiv.removeChild(square)
-    });
-    genGrid();
-})
+window.onload = () => {
+    containerDiv.style.cssText = `height: ${CONTAINER_DIM}px; width: ${CONTAINER_DIM}px`;
+    genGrid(inputResolution.value)
+}
