bae597f70468173780f518769f99b3cfac0625d8
Louis Vannoorenberghe <louis.vannoo@gmail.com>
2022-12-05 21:42:27 +0100

    add multicolor mode

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const CONTAINER_DIM = 900;
+const CONTAINER_DIM = 800;
 
 const containerDiv = document.querySelector("div.container");
 const inputResolution = document.querySelector("input#res");
 const resetButton = document.querySelector("button#reset");
 const colorSelector = document.querySelector("input#colorSelector");
+const multicolorCheckbox = document.querySelector("input#multicolor")
 
+let randomR;
+let randomG;
+let randomB;
 let mouseDown = false;
+let color = colorSelector.value;
 
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
@@ -19,7 +24,7 @@ function genGrid(resolution) {
     squareDiv.classList.add('square'); 
     squareDiv.style.cssText = `height: ${CONTAINER_DIM/resolution}px; width: ${CONTAINER_DIM/resolution}px`;
     containerDiv.appendChild(squareDiv);
-}
+    }
 }
 
 function cleanGrid(){
@@ -27,14 +32,23 @@ function cleanGrid(){
 }
 
 function changeColor(e){
-    if (e.type === 'mouseover' && mouseDown)
-        {e.target.style.backgroundColor = colorSelector.value}
+    if (e.type === 'mouseover' && mouseDown && e.target != containerDiv) {
+        if (multicolorCheckbox.checked) {
+            randomR = Math.round(Math.random() * 256);
+            randomG = Math.round(Math.random() * 256);
+            randomB = Math.round(Math.random() * 256);
+            color = `rgb(${randomR}, ${randomG}, ${randomB})`
+        }
+        else {color = colorSelector.value}
+        e.target.style.backgroundColor = color;
+    }
 }
 
 function resetGrid(){
     cleanGrid();
     genGrid(inputResolution.value);
 }
+
 
 window.onload = () => {
     containerDiv.style.cssText = `height: ${CONTAINER_DIM}px; width: ${CONTAINER_DIM}px`;
